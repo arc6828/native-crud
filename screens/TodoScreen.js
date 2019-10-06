8b1421443c4352a5f88cb9a656cdc618f574f6e8
arc6828 <chavalit.kow@gmail.com>
2019-10-06 23:40:13 +0700

    Commit

--- screens/TodoScreen.js
+++ screens/TodoScreen.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import TodosContainer from '../components/TodosContainer';
 
+import { StyleSheet, View,  Platform, TouchableOpacity, ScrollView,  FlatList, AsyncStorage, Text, TextInput } from 'react-native';
+
+import * as firebase from 'firebase';
+import '@firebase/firestore';
+
+
 export default function TodoScreen() {
 
   return  < TodosContainer/> ;
 }
 
+
 TodoScreen.navigationOptions = {
   title: 'Todo',
+  headerRight: (
+    <Text>
+    </Text>
+  ),
 };
 
 
