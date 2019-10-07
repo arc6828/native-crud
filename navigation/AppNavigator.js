81aeef929424a3ba9479c98a2449961587156717
arc6828 <chavalit.kow@gmail.com>
2019-10-07 08:46:24 +0700

    Has Login / Register

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator,createStackNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 
-import LoginScreen from '../screen/LoginScreen';
-import RegisterScreen from '../screen/RegisterScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import AuthLoadingScreen from '../screens/AuthLoadingScreen';
 
 import {
   ActivityIndicator,
@@ -31,32 +32,5 @@ export default createAppContainer(
     initialRouteName: 'AuthLoading',
   })
 );
-
-
-
-class AuthLoadingScreen extends React.Component {
-  componentDidMount() {
-    this._bootstrapAsync();
-  }
-
-  // Fetch the token from storage then navigate to our appropriate place
-  _bootstrapAsync = async () => {
-    const user = await AsyncStorage.getItem('user');
-
-    // This will switch to the App screen or Auth screen and this loading
-    // screen will be unmounted and thrown away.
-    this.props.navigation.navigate(user ? 'Main' : 'Auth');
-  };
-
-  // Render any loading content that you like here
-  render() {
-    return (
-      <View>
-        <ActivityIndicator />
-        <StatusBar barStyle="default" />
-      </View>
-    );
-  }
-}
 
 
